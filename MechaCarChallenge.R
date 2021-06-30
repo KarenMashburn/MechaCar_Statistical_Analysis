c2f9ff3029fdae4fc969f9a5183c268967da34cb
KarenMashburn <79940917+KarenMashburn@users.noreply.github.com>
2021-06-30 10:04:01 -0500

    Deliverable 1.

---
+++ MechaCarChallenge.R
@@ -0,0 +1,9 @@
+library(dplyr)
+
+MechaCar_mpg_table <- read.csv(file = "MechaCar_mpg.csv",check.names=F,stringsAsFactors = F)
+
+lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg_table)
+
+summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg_table))
+
+
